31ca58700f3f423e2c02c32125ffadf95d71750f
Evelín  Banega <banegaevelin2025@gmail.com>
2026-02-07 12:31:35 +0100

    Use Task class in Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Java OOP project - work in progress");
-    }
 
+        Task task1 = new Task("Learn Java OOP");
+        Task task2 = new Task("Create GitHub project");
+
+        task1.markAsCompleted();
+
+        System.out.println(task1.getTitle() + " - completed: " + task1.isCompleted());
+        System.out.println(task2.getTitle() + " - completed: " + task2.isCompleted());
+    }
 }
